d59d39d40eb91ccd820c08089cbc0e448de810c3
Tien-Yiao Hsu <tienyiah@lundey.ess.uci.edu>
2018-09-25 02:18:30 -0700

    Generate download

--- web/main.py
+++ web/main.py
@@ -1,13 +1,15 @@
 from bottle import route, run, template, static_file
 import os.path
 
-root = os.path.dirname(os.path.realpath(__file__))
+root_dir = os.path.dirname(os.path.realpath(__file__))
+
 @route('/static/<filename>')
 def server_static(filename):
-    return static_file(filename, root=os.path.join(root, "static"))
+    return static_file(filename, root=os.path.join(root_dir, "static"))
 
 @route('/')
 def root():
+    print("ROOT")
     return template("index")
 
 
